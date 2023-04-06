ee10ca388fa457568a708e587829a59c0a621311
Aaron <aaronchan289@gmail.com>
2023-04-06 16:36:36 +0800

    modify

--- widgets/parts.py
+++ widgets/parts.py
@@ -56,6 +56,29 @@ class MedianFrame(ttk.LabelFrame):
         self.radiobutton4.pack()
         self.radioStringVar.set('red')
         
-    
+        checkFrames = ttk.LabelFrame(self, text='Check Buttons')
+        checkFrames.pack(side=tk.RIGHT, padx=10, pady=10)
+
+        self.checkStringVar1 = tk.StringVar()
+        self.checkStringVar2 = tk.StringVar()
+        self.checkStringVar3 = tk.StringVar()
+        self.checkStringVar4 = tk.StringVar()
+
+        self.checkbutton1 = ttk.Checkbutton(checkFrames, text='Option 1',variable=self.checkStringVar1,command=self.checkEvent,onvalue='op1check',offvalue='op1off')
+        self.checkbutton1.pack()
+        self.checkbutton2 = ttk.Checkbutton(checkFrames, text='Option 2',variable=self.checkStringVar2,command=self.checkEvent,onvalue='op2check',offvalue='op2off')
+        self.checkbutton2.pack()
+        self.checkbutton3 = ttk.Checkbutton(checkFrames, text='Option 3', variable=self.checkStringVar3,command=self.checkEvent,onvalue='op3check',offvalue='op3off')
+        self.checkbutton3.pack()
+        self.checkbutton4 = ttk.Checkbutton(checkFrames, text='Option 4', variable=self.checkStringVar4,command=self.checkEvent,onvalue='op4check',offvalue='op4off')
+        self.checkbutton4.pack()
+        
+
     def radioEvent(self):
         self.w.radioButtonEvent0fMedianFrame(self.radioStringVar.get())
+
+    def checkEvent(self):
+        print(self.checkStringVar1.get())
+        print(self.checkStringVar2.get())
+        print(self.checkStringVar3.get())
+        print(self.checkStringVar4.get())
