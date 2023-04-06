7ae7f64c3c98b4aadbfd3977a029919f5ca3b6bb
Aaron <aaronchan289@gmail.com>
2023-04-06 13:50:13 +0800

    modify

--- widgets/main.py
+++ widgets/main.py
@@ -1,20 +1,12 @@
 import tkinter as tk
-from tkinter import ttk
-from PIL import Image, ImageTk
+from parts import TopFrame
 
-def createImage0fTkinterUsingPIL():
-    pass
+
 
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
-        topFrame = ttk.LabelFrame(self)
-        flowerImage1 = Image.open("./images/flower1.png")
-        self.flowerPhoto1 = ImageTk.PhotoImage(flowerImage1)
-        canvas = tk.Canvas(topFrame, width=173, height=200)
-        canvas.pack()
-        canvas.create_image(0,0,image=self.flowerPhoto1,anchor='nw')
-        canvas.create_text(0,200,text='Flower', fill='yellow', font=('verdana', 36),anchor='sw')
+        topFrame = TopFrame(self,borderwidth=0)
         topFrame.pack()
 
 
---
+++ widgets/parts.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
+
+class TopFrame(ttk.LabelFrame):
+    def __init__(self,master,**kwargs):
+        super().__init__(master,**kwargs)
+        flowerImage1 = Image.open("./images/flower1.png")
+        self.flowerPhoto1 = ImageTk.PhotoImage(flowerImage1)
+        self.canvas = tk.Canvas(self, width=173, height=200)
+        self.canvas.pack()
+        self.canvas.create_image(0,5,image=self.flowerPhoto1,anchor='nw')
+        self.canvas.create_text(0,200,text='Flower', fill='yellow', font=('verdana', 36),anchor='sw')
+
+
+        diamondImage1 = Image.open("./images/diamond.png")
+        self.diamondPhoto1 = ImageTk.PhotoImage(diamondImage1)
+        self.canvas.create_image(175,5,image=self.diamondPhoto1,anchor='nw')
+
+
+        atomImage1 = Image.open("./images/atom.png")
+        self.atomPhoto1 = ImageTk.PhotoImage(atomImage1)
+        self.canvas.create_image(280,5,image=self.atomPhoto1,anchor='nw')
+
+
+        self.scrollbar = ttk.Scrollbar(self, orient = 'horizontal', command=self.canvas.xview)
+        self.scrollbar.pack(side='bottom', fill='x')
+        self.canvas.configure(xscrollcommand=self.scrollbar.set)
+        self.canvas.configure(scrollregion=(0,0,500,200))
