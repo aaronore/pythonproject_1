4a1db6ad068c6fef088139a748e5ca22b0f862ed
Aaron <aaronchan289@gmail.com>
2023-04-12 14:55:17 +0800

    modify

--- youbike/datasource.py
+++ youbike/datasource.py
@@ -1,9 +1,10 @@
 import requests
 
+#Store the area names and data
 sarea_list=None
 data_list =None
 
-def getInfo():
+def getInfo() -> None:
     global sarea_list,data_list
     url ="https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
     respones = requests.get(url)
@@ -18,5 +19,12 @@ def getInfoFromArea(areaName) -> list:
     filter_data = filter(lambda n:n["sarea"]==areaName,data_list)
     return list(filter_data)
 
+def filter_sbi_warning_data(area_data,numbers) -> list:
+    filter_data = filter(lambda n:n['sbi']<=numbers,area_data)
+    return list(filter_data)
+
+def filter_bemp_warning_data(area_data, numbers) -> list:
+    filter_data = filter(lambda n: n['bemp'] <= numbers, area_data)
+    return list(filter_data)
 
 getInfo()
--- youbike/main.py
+++ youbike/main.py
@@ -5,6 +5,9 @@ import datasource
 import tkinter as tk
 from tkinter import ttk
 
+sbi_numbers =3
+bemp_numbers =3
+
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -12,6 +15,7 @@ class Window(tk.Tk):
 #top_wrapperFrame=================
         top_wrapperFrame = ttk.Frame(self)
         top_wrapperFrame.pack(fill=tk.X)
+#topFrame_start===================
 
 #topFrame_start------------------------------------------
         topFrame = ttk.LabelFrame(self,text="台北市行政區")
@@ -21,16 +25,44 @@ class Window(tk.Tk):
             cols = i % 3
             rows = i //3
             ttk.Radiobutton(topFrame,text=datasource.sarea_list[i],value=datasource.sarea_list[i],variable=self.radioStringVar,command=self.radio_Event).grid(column=cols,row=rows,sticky=tk.W,padx=10,pady=10)    
-        topFrame.pack()
+        topFrame.pack(side=tk.LEFT)
         self.radioStringVar.set('信義區')
         self.area_data = datasource.getInfoFromArea('信義區')
 #topFrame_end----------------------------------------------------
 
-#warningFrame_start-----------------------------------------------
-        sbi_warningFrame = ttk.LabelFrame(top_wrapperFrame,text="車位目前不足站點")
-        warning_sbi_Frame = ttk.Frame
-        warning__sbi_Frame.pack()
-        warningFrame.pack(side=tk.LEFT)
+        #sbi_warningFrame_start====================
+        sbi_warningFrame = ttk.LabelFrame(top_wrapperFrame,text="可借目前不足站點")       
+        columns = ('#1','#2','#3')
+        self.sbi_tree =ttk.Treeview(sbi_warningFrame, columns=columns, show='headings')
+        self.sbi_tree.heading('#1', text='站點')
+        self.sbi_tree.column("#1", minwidth=0, width=200)        
+        self.sbi_tree.heading('#2', text='可借')
+        self.sbi_tree.column("#2", minwidth=0, width=30)
+        self.sbi_tree.heading('#3', text='可還')
+        self.sbi_tree.column("#3", minwidth=0, width=30)        
+        self.sbi_tree.pack(side=tk.LEFT)
+        self.sbi_warning_data = datasource.filter_sbi_warning_data(self.area_data,sbi_numbers)
+        for item in self.sbi_warning_data:
+            self.sbi_tree.insert('',tk.END,values=[item['sna'][11:],item['sbi'],item['bemp']])
+        sbi_warningFrame.pack(side=tk.LEFT)
+        #sbi_warningFrame_end======================
+
+        #bemp_warningFrame_start====================
+        bemp_warningFrame = ttk.LabelFrame(top_wrapperFrame,text="可還目前不足站點")       
+        columns = ('#1', '#2', '#3')
+        self.bemp_tree = ttk.Treeview(bemp_warningFrame, columns=columns, show='headings')
+        self.bemp_tree.heading('#1', text='站點')
+        self.bemp_tree.column("#1", minwidth=0, width=200)        
+        self.bemp_tree.heading('#2', text='可借')
+        self.bemp_tree.column("#2", minwidth=0, width=30)
+        self.bemp_tree.heading('#3', text='可還')
+        self.bemp_tree.column("#3", minwidth=0, width=30)        
+        self.bemp_tree.pack(side=tk.LEFT)
+        self.bemp_warning_data = datasource.filter_bemp_warning_data(self.area_data,bemp_numbers)
+        for item in self.bemp_warning_data:
+            self.bemp_tree.insert('',tk.END,values=[item['sna'][11:],item['sbi'],item['bemp']])
+        bemp_warningFrame.pack(side=tk.LEFT)
+        #bemp_warningFrame_end======================
 
         bottomFrame = ttk.LabelFrame(self,text=self.radioStringVar.get())
         bottomFrame.pack()
@@ -51,7 +83,7 @@ class Window(tk.Tk):
         self.tree.column("#6", minwidth=0, width=250)
         self.tree.heading('#7', text='狀態')
         self.tree.column("#7", minwidth=0, width=30)
-        self.tree.pack(side=tk.LEFT,fill=tk.Y)
+        self.tree.pack(side=tk.LEFT)
 
         for item in self.area_data:
             self.tree.insert('',tk.END,values=[item['sna'][11:],item['mday'],item['tot'],item['sbi'],item['bemp'],item['ar'],item['act']])
@@ -66,8 +98,24 @@ class Window(tk.Tk):
         
         for item in self.tree.get_children():
             self.tree.delete(item)
+
+        #Get selected radio button value    
         area_name = self.radioStringVar.get()
+        
+        #Get all station data from selected area
         self.area_data = datasource.getInfoFromArea(area_name)
+
+        # Filter data with sbi warning number
+        self.sbi_warning_data = datasource.filter_sbi_warning_data(self.area_data,sbi_numbers)
+        print("sbi:")
+        print(self.sbi_warning_data)
+        
+        # Filter data with bemp warning number
+        self.bemp_waring_data = datasource.filter_bemp_warning_data(self.area_data,bemp_numbers)
+        print("bemp:")
+        print(self.bemp_waring_data)
+
+        # Display data in tree view
         for item in self.area_data:
             #print(item) 印看
             self.tree.insert('',tk.END,values=[item['sna'][11:],item['mday'],item['tot'],item['sbi'],item['bemp'],item['ar'],item['act']])
